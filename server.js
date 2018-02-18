01d50d4146b1e76054227a56dfeb7622cee0de0a
ivanevacic <ivanevacicweb@gmail.com>
2018-02-18 18:22:55 +0100

    Created  const that 'simulates' db,for testing purposes

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require('express');
+
 //  Create app buy running express
 const app = express();
 
+//  Mock DB varible
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'Ivan',
+      email: 'ivanevacicweb@gmail.com',
+      password: 'cookies',
+      //  How many times user submitted photo
+      entries: 0,
+      //  Date when user joined
+      joined: new Date()
+    },
+    {
+      id: '456',
+      name: 'Marko',
+      email: 'marko1@gmail.com',
+      password: 'bananas',
+      entries: 1,
+      joined: new Date()
+    }
+  ]
+}
+
 app.get('/', (req, res)=>{
   res.send('this is working');
+})
+
+app.post('/signin', (req, res) => {
+  //  We get json string
+  res.json('signin route works');
 })
 
 app.listen(3000, () => {
